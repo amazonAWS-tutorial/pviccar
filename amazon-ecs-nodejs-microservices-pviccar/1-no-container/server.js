0bd4e403e4b2d4893ee3fabe7cf8e622209223c9
amazonAWS-tutorial <67293996+amazonAWS-tutorial@users.noreply.github.com>
2020-06-23 11:21:37 +0200

    first commit

---
+++ amazon-ecs-nodejs-microservices-pviccar/1-no-container/server.js
@@ -0,0 +1,52 @@
+const app = require('koa')();
+const router = require('koa-router')();
+const db = require('./db.json');
+
+// Log requests
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+router.get('/pviccar/users-pviccar', function *(next) {
+  this.body = db.users;
+});
+
+router.get('/pviccar/users-pviccar/:userId', function *(next) {
+  const id = parseInt(this.params.userId);
+  this.body = db.users.find((user) => user.id == id);
+});
+
+router.get('/pviccar/threads-pviccar', function *() {
+  this.body = db.threads;
+});
+
+router.get('/pviccar/threads-pviccar/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.threads.find((thread) => thread.id == id);
+});
+
+router.get('/pviccar/posts-pviccar/in-thread/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.posts.filter((post) => post.thread == id);
+});
+
+router.get('/pviccar/posts-pviccar/by-user/:userId', function *() {
+  const id = parseInt(this.params.userId);
+  this.body = db.posts.filter((post) => post.user == id);
+});
+
+router.get('/pviccar/', function *() {
+  this.body = "API ready to receive requests";
+});
+
+router.get('/', function *() {
+  this.body = "Ready to receive requests";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
